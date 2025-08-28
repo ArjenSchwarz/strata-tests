542c71894644975483ee8e2b215a14b437b8df5a
Arjen Schwarz <developer@arjen.eu>
2025-08-28 11:00:57 +1000

    Add a test for subdirectory

--- subdirtest/main.tf
+++ subdirtest/main.tf
@@ -6,17 +6,17 @@ terraform {
 
 resource "local_file" "test_files" {
   count    = 2
-  filename = "${path.module}/file${count.index + 1}.txt"
+  filename = "${path.module}/subdir/file${count.index + 1}.txt"
   content  = ""
 }
 
 resource "local_file" "other_files" {
   count    = 2
-  filename = "${path.module}/file${count.index + 1}.txt"
+  filename = "${path.module}/subdir/other_file${count.index + 1}.txt"
   content  = ""
 }
 
 output "test_file_names" {
-  description = "Names of the created test files"
+  description = "Names of the created subdir test files"
   value       = local_file.test_files[*].filename
 }
